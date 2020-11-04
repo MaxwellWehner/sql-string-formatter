459ba8d4946482ee383837f2bfb5cb220da9e225
Max Wehner <maxwehnerwork@gmail.com>
2020-11-04 00:37:35 -0700

    changed defalut drop down to set of radio buttons and added styling

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ const App = () => {
     const prependValue = document.getElementById("prepend");
     const appendValue = document.getElementById("append");
     const delimiterValue = document.getElementById("delimiter");
-    const classType = document.getElementsByName("caseType");
+    const caseType = document.getElementsByName("caseType");
     const inputData = document.getElementsByClassName("main-input")[0];
     const trimedChecked = document.getElementById("trimCheck");
+    const defaults = document.getElementsByName("defaults");
 
     setWrappings({
       prepend: `${prependValue.value}`,
@@ -25,8 +26,8 @@ const App = () => {
     setInputData(inputData.value);
     setIsChecked(trimedChecked.checked);
     for (let i = 0; i < 3; i++) {
-      if (classType[i].checked) {
-        switch (classType[i].id) {
+      if (caseType[i].checked) {
+        switch (caseType[i].id) {
           case "none":
             setTextCase("none");
             break;
@@ -42,44 +43,44 @@ const App = () => {
         }
       }
     }
-  };
 
-  const setDefalutDropDown = () => {
-    const prependValue = document.getElementById("prepend");
-    const appendValue = document.getElementById("append");
-    const defaltDropDown = document.getElementById("defaults");
-
-    switch (defaltDropDown.options[defaltDropDown.selectedIndex].value) {
-      case '"':
-        setWrappings({ prepend: '"', append: '"' });
-        prependValue.value = '"';
-        appendValue.value = '"';
-        break;
-      case "'":
-        setWrappings({ prepend: "'", append: "'" });
-        prependValue.value = "'";
-        appendValue.value = "'";
-        break;
-      case "()":
-        setWrappings({ prepend: "(", append: ")" });
-        prependValue.value = "(";
-        appendValue.value = ")";
-        break;
-      case "[]":
-        setWrappings({ prepend: "[", append: "]" });
-        prependValue.value = "[";
-        appendValue.value = "]";
-        break;
-      case "":
-        setWrappings({ prepend: "", append: "" });
-        prependValue.value = "";
-        appendValue.value = "";
-        break;
-      default:
-        setWrappings({ prepend: "", append: "" });
-        prependValue.value = "";
-        appendValue.value = "";
-        break;
+    for (let i = 0; i < 6; i++) {
+      if (defaults[i].checked) {
+        switch (defaults[i].id) {
+          case "typeIn":
+            break;
+          case "blank":
+            setWrappings({ prepend: "", append: "" });
+            prependValue.value = "";
+            appendValue.value = "";
+            break;
+          case "'":
+            setWrappings({ prepend: "'", append: "'" });
+            prependValue.value = "'";
+            appendValue.value = "'";
+            break;
+          case '"':
+            setWrappings({ prepend: '"', append: '"' });
+            prependValue.value = '"';
+            appendValue.value = '"';
+            break;
+          case "()":
+            setWrappings({ prepend: "(", append: ")" });
+            prependValue.value = "(";
+            appendValue.value = ")";
+            break;
+          case "[]":
+            setWrappings({ prepend: "[", append: "]" });
+            prependValue.value = "[";
+            appendValue.value = "]";
+            break;
+          default:
+            setWrappings({ prepend: "", append: "" });
+            prependValue.value = "";
+            appendValue.value = "";
+            break;
+        }
+      }
     }
   };
 
@@ -134,81 +135,131 @@ const App = () => {
     document.execCommand("copy");
   };
 
+  const setToTypeIn = () => {
+    const typeIn = document.getElementById("typeIn");
+    typeIn.checked = true;
+  };
+
   return (
     <div className="App">
-      <form>
-        <label htmlFor="prepend">Prepend: </label>
-        <input
-          type="text"
-          id="prepend"
-          defaultValue={wrappings.prepend}
-          onChange={setUserValues}
-        />
-        <br />
-        <label htmlFor="append">Append: </label>
-        <input
-          type="text"
-          id="append"
-          defaultValue={wrappings.append}
-          onChange={setUserValues}
-        />
-        <br />
-        <label htmlFor="delimeter">Delimiter: </label>
-        <input
-          type="text"
-          id="delimiter"
-          defaultValue={delimiter}
-          onChange={setUserValues}
-        />
-      </form>
-      <form>
-        <p>Select Case Type:</p>
-        <label htmlFor="none">No Change:</label>
-        <input
-          type="radio"
-          id="none"
-          name="caseType"
-          defaultChecked={true}
-          onChange={setUserValues}
-        />
-        <br />
-        <label htmlFor="LowerCase">Lower Case:</label>
-        <input
-          type="radio"
-          id="LowerCase"
-          name="caseType"
-          onChange={setUserValues}
-        />
-        <br />
-        <label htmlFor="UpperCase">Upper Case:</label>
-        <input
-          type="radio"
-          id="UpperCase"
-          name="caseType"
-          onChange={setUserValues}
-        />
-      </form>
-      <br />
-      <input type="checkbox" id="trimCheck" onChange={setUserValues} />
-      <label htmlFor="trimCheckBox">Trim WhiteSpace</label>
-      <br />
-      <label htmlFor="defaults">Select a defalut prefix and suffix: </label>
-      <select name="defaults" id="defaults" onChange={setDefalutDropDown}>
-        <option value="">Prepend/Append: nothing</option>
-        <option value="'">Prepend/Append: 'single quote'</option>
-        <option value='"'>Prepend/Append: "double quote"</option>
-        <option value="()">Prepend/Append: (parentheses)</option>
-        <option value="[]">Prepend/Append: [square brackets]</option>
-      </select>
-      <br />
-      <div className="buttons">
-        <button onClick={convertText}>Convert Text</button>
-        <br />
-        <button onClick={clearBoxes}>Clear All</button>
-        <br />
-        <button onClick={copyOutput}>Add to clipboard</button>
+      <div className="formContainer">
+        <div className="formTop">
+          <form className="attach">
+            <label htmlFor="prepend">Prepend: </label>
+            <input
+              type="text"
+              id="prepend"
+              defaultValue={wrappings.prepend}
+              onChange={setUserValues}
+              onClick={setToTypeIn}
+            />
+            <label htmlFor="append">Append: </label>
+            <input
+              type="text"
+              id="append"
+              defaultValue={wrappings.append}
+              onChange={setUserValues}
+              onClick={setToTypeIn}
+            />
+            <label htmlFor="delimeter">Delimiter: </label>
+            <input
+              type="text"
+              id="delimiter"
+              defaultValue={delimiter}
+              onChange={setUserValues}
+            />
+          </form>
+          <form className="case">
+            <label>Select Case Type:</label>
+
+            <input
+              type="radio"
+              id="none"
+              name="caseType"
+              defaultChecked={true}
+              onChange={setUserValues}
+            />
+            <label htmlFor="none">No Change</label>
+
+            <input
+              type="radio"
+              id="LowerCase"
+              name="caseType"
+              onChange={setUserValues}
+            />
+            <label htmlFor="LowerCase">Lower Case</label>
+
+            <input
+              type="radio"
+              id="UpperCase"
+              name="caseType"
+              onChange={setUserValues}
+            />
+            <label htmlFor="UpperCase">Upper Case</label>
+          </form>
+          <form className="defaults">
+            <label>Select a defalut prefix and suffix:</label>
+
+            <input
+              type="radio"
+              id="typeIn"
+              name="defaults"
+              defaultChecked={true}
+              onChange={setUserValues}
+            />
+            <label htmlFor="typeIn">Type In</label>
+
+            <input
+              type="radio"
+              id="blank"
+              name="defaults"
+              onChange={setUserValues}
+            />
+            <label htmlFor="blank">Blank</label>
+
+            <input
+              type="radio"
+              id="'"
+              name="defaults"
+              onChange={setUserValues}
+            />
+            <label htmlFor="'">'single quote'</label>
+
+            <input
+              type="radio"
+              id='"'
+              name="defaults"
+              onChange={setUserValues}
+            />
+            <label htmlFor='"'>"double quote"</label>
+
+            <input
+              type="radio"
+              id="()"
+              name="defaults"
+              onChange={setUserValues}
+            />
+            <label htmlFor="()">(parentheses)</label>
+
+            <input
+              type="radio"
+              id="[]"
+              name="defaults"
+              onChange={setUserValues}
+            />
+            <label htmlFor="[]">[brackets]</label>
+          </form>
+          <input type="checkbox" id="trimCheck" onChange={setUserValues} />
+          <label htmlFor="trimCheck">Trim WhiteSpace</label>
+        </div>
+        <div className="buttons">
+          <button onClick={convertText}>Convert Text</button>
+          <br />
+          <button onClick={copyOutput}>Add to clipboard</button>
+          <br />
+          <button onClick={clearBoxes}>Clear All</button>
+        </div>
       </div>
-      <br />
       <textarea
         type="text"
         className="main-input"
